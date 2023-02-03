713ce739ed8da08b935ff2ad1381de16729fa5bf
Anjali197 <anjalics101@gmail.com>
2023-02-03 16:20:02 +0530

    Update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginationModule } from './features/pagination/pagination/pagination.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pagination' , loadChildren:()=>import('./features/pagination/pagination/pagination.module').then(m=>m.PaginationModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
